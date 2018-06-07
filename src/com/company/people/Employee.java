1a34aec32cab1201a851f1b7e09ce0f21e9e6d17
Bin Lin <binlin2000@gmail.com>
2018-06-07 14:18:01 -0400

    added people package

---
+++ src/com/company/people/Employee.java
@@ -0,0 +1,43 @@
+package com.company.people;
+
+public class Employee extends People {
+    private int employeeNUmber;
+    private String jobTitle;
+    private String hireDay;
+    private int Hours;
+
+    public Employee(int age, String name, char gender, String race, int employeeNUmber, String jobTitle, String hireDay) {
+        super(age, name, gender, race);
+        this.employeeNUmber = employeeNUmber;
+        this.jobTitle = jobTitle;
+        this.hireDay = hireDay;
+    }
+
+    public int getEmployeeNUmber() {
+        return employeeNUmber;
+    }
+
+    public void setEmployeeNUmber(int employeeNumber) {
+        this.employeeNUmber = employeeNumber;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getHireDay() {
+        return hireDay;
+    }
+
+    public int getHours() {
+        return Hours;
+    }
+
+    public void setHours(int hours) {
+        Hours = hours;
+    }
+}
---
+++ src/com/company/people/Visitor.java
@@ -0,0 +1,47 @@
+package com.company.people;
+
+public class Visitor extends People{
+    private int groupSize;
+    private String allergies;
+    private boolean membership;
+    private String homeAddress;
+
+    public Visitor(int age, String name, char gender, String race, int groupSize, boolean membership, String homeAddress) {
+        super(age, name, gender, race);
+        this.groupSize = groupSize;
+        this.membership = membership;
+        this.homeAddress = homeAddress;
+    }
+
+    public int getGroupSize() {
+        return groupSize;
+    }
+
+    public void setGroupSize(int groupSize) {
+        this.groupSize = groupSize;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public boolean isMembership() {
+        return membership;
+    }
+
+    public void setMembership(boolean membership) {
+        this.membership = membership;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+}
